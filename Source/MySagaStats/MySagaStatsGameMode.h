16d8ba70662c7f6470cdcaa4a45b08df680148e2
ZhangJinming <zjm545237835@gmail.com>
2024-08-19 16:28:54 +0800

    初始化

---
+++ Source/MySagaStats/MySagaStatsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MySagaStatsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMySagaStatsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMySagaStatsGameMode();
+};
+
+
+
