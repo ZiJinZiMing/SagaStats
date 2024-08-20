bb3495040e22a33b9bb363eae68f2359adacce6d
ZhangJinming <zjm545237835@gmail.com>
2024-08-20 10:02:02 +0800

    添加GameplayAbilities插件 新建SagaStats插件

---
+++ Plugins/SagaStats/Source/SagaStats/Private/SagaStats.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SagaStats.h"
+
+#define LOCTEXT_NAMESPACE "FSagaStatsModule"
+
+void FSagaStatsModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FSagaStatsModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSagaStatsModule, SagaStats)
