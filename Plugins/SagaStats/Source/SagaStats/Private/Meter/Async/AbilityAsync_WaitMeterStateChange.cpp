a4511ab87978c97a3f719d162d2d042738bde29f
ZhangJinming <zjm545237835@gmail.com>
2024-12-25 18:46:30 +0800

    添加UI_MeterBar

---
+++ Plugins/SagaStats/Source/SagaStats/Private/Meter/Async/AbilityAsync_WaitMeterStateChange.cpp
@@ -0,0 +1,46 @@
+﻿/******************************************************************************************
+* Plugin:       SagaStats
+* Author:       Jinming Zhang
+* Description:  SagaStats is a status system that supports fully blueprintable attribute definitions and value calculations.
+******************************************************************************************/
+
+
+#include "Meter/Async/AbilityAsync_WaitMeterStateChange.h"
+
+#include "SagaAbilitySystemComponent.h"
+#include "Meter/SagaDecreaseMeter.h"
+
+UAbilityAsync_WaitMeterStateChange* UAbilityAsync_WaitMeterStateChange::WaitMeterStateChange(AActor* TargetActor, TSubclassOf<USagaDecreaseMeter> MeterClass)
+{
+	UAbilityAsync_WaitMeterStateChange* Async = NewObject<UAbilityAsync_WaitMeterStateChange>();
+	Async->SetAbilityActor(TargetActor);
+	Async->MeterClass = MeterClass;
+	return Async;
+}
+
+void UAbilityAsync_WaitMeterStateChange::Activate()
+{
+	Super::Activate();
+	if (USagaAbilitySystemComponent* ASC = Cast<USagaAbilitySystemComponent>(GetAbilitySystemComponent()))
+	{
+		MeterStateChangeHandle = ASC->GetMeterStateChangeDelegate(MeterClass).AddUObject(this, &ThisClass::OnMeterStateChangeCallback);
+	}
+	else
+	{
+		EndAction();
+	}
+}
+
+void UAbilityAsync_WaitMeterStateChange::EndAction()
+{
+	if (USagaAbilitySystemComponent* ASC = Cast<USagaAbilitySystemComponent>(GetAbilitySystemComponent()))
+	{
+		ASC->GetMeterStateChangeDelegate(MeterClass).Remove(MeterStateChangeHandle);
+	}
+	Super::EndAction();
+}
+
+void UAbilityAsync_WaitMeterStateChange::OnMeterStateChangeCallback(USagaDecreaseMeter* Meter, EMeterState OldState)
+{
+	OnStateChange.Broadcast(Meter, Meter->MeterState, OldState);
+}
---
+++ Plugins/SagaStats/Source/SagaStats/Public/Meter/Async/AbilityAsync_WaitMeterStateChange.h
@@ -0,0 +1,46 @@
+﻿/******************************************************************************************
+* Plugin:       SagaStats
+* Author:       Jinming Zhang
+* Description:  SagaStats is a status system that supports fully blueprintable attribute definitions and value calculations.
+******************************************************************************************/
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Async/AbilityAsync.h"
+#include "SagaStatsType.h"
+#include "AbilityAsync_WaitMeterStateChange.generated.h"
+
+
+class USagaDecreaseMeter;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnWaitMeterStateChangeEvent, USagaDecreaseMeter*, Meter, EMeterState, NewState,EMeterState, OldState);
+
+/**
+ * 
+ */
+UCLASS()
+class SAGASTATS_API UAbilityAsync_WaitMeterStateChange : public UAbilityAsync
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FOnWaitMeterStateChangeEvent OnStateChange;
+
+	UFUNCTION(BlueprintCallable, Category = "Ability|Async", meta = (DefaultToSelf = "TargetActor", BlueprintInternalUseOnly = "TRUE"))
+	static UAbilityAsync_WaitMeterStateChange* WaitMeterStateChange(AActor* TargetActor, TSubclassOf<USagaDecreaseMeter> MeterClass);
+
+protected:
+	virtual void Activate() override;
+
+	virtual void EndAction() override;
+
+private:
+	FDelegateHandle MeterStateChangeHandle;
+
+	UPROPERTY()
+	TSubclassOf<USagaDecreaseMeter> MeterClass;
+
+	UFUNCTION()
+	void OnMeterStateChangeCallback(USagaDecreaseMeter* Meter,EMeterState OldState);
+};
